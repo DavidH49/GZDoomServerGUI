16d723ab062e07c74c996db559ce7e4b24b35d77
DavidH49 <david.heilmannseder@cometomail.de>
2023-08-25 11:44:29 +0200

    Added environment variables

---
+++ GZDoomServerGUI/InputReader.cs
@@ -0,0 +1,43 @@
+﻿namespace GZDoomServerGUI;
+
+public partial class MainWindow
+{
+    // Checks which textboxes have been set and adds their values to the command
+    private List<string> GetAllHostInputs()
+    {
+        var config = new List<string>();
+
+        if (textWadName.Text.Trim() != string.Empty)
+            config.Add($"-iwad {textWadName.Text.Trim()}");
+
+        if (textboxNPlayer.Text.Trim() != string.Empty)
+            config.Add($"-host {textboxNPlayer.Text.Trim()}");
+
+        if (textboxSkill.Text.Trim() != string.Empty)
+            config.Add($"-skill {textboxSkill.Text.Trim()}");
+
+        if (textboxLevel.Text.Trim() != string.Empty)
+            config.Add($"-warp {textboxLevel.Text.Trim()}");
+
+        if (checkDeathmatch.Checked)
+            config.Add("-deathmatch");
+
+        if (!checkMonsters.Checked)
+            config.Add("-nomonsters");
+
+        return config;
+    }
+
+    // Checks which textboxes have been set and adds their values to the command
+    private List<string> GetAllJoinInputs()
+    {
+        var config = new List<string>();
+
+        if (textboxWadJoin.Text.Trim() != string.Empty)
+            config.Add($"-iwad {textboxWadJoin.Text.Trim()}");
+
+        config.Add($"-join {textboxHostIp.Text.Trim()}");
+
+        return config;
+    }
+}
--- GZDoomServerGUI/MainWindow.cs
+++ GZDoomServerGUI/MainWindow.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+
 namespace GZDoomServerGUI;
 
 public partial class MainWindow : Form
@@ -7,68 +10,25 @@ public partial class MainWindow : Form
         InitializeComponent();
     }
 
-    // If it can't find environment variable "GZDOOM", it defaults to the path of the dev's GZDoom installation
-    private string GzDoom
-    {
-        get
-        {
-            string? env = Environment.GetEnvironmentVariable("GZDOOM");
-            return env ?? @"C:\Apps\DOOM\gzdoom\gzdoom.exe";
-        }
-    }
+    // Tries to get the environment variable GZDOOM
+    private string? GzDoom
+        => Environment.GetEnvironmentVariable("GZDOOM");
 
     private void btnHostServer_Click(object sender, EventArgs e)
     {
-        List<string> config = GetAllHostInputs();
-        string configStr = String.Join(" ", config.ToArray());
+        string configStr = String.Join(
+            " ",
+            GetAllHostInputs().ToArray());
 
         System.Diagnostics.Process.Start(GzDoom, configStr);
     }
 
     private void btnJoinServer_Click(object sender, EventArgs e)
     {
-        var config = GetAllJoinInputs();
-        string configStr = String.Join(" ", config.ToArray());
+        string configStr = String.Join(
+            " ",
+            GetAllJoinInputs().ToArray());
 
         System.Diagnostics.Process.Start(GzDoom, configStr);
-    }
-
-    // Checks which textboxes have been set and adds their values to the command
-    private List<string> GetAllHostInputs()
-    {
-        var config = new List<string>();
-
-        if (textWadName.Text.Trim() != string.Empty)
-            config.Add($"-iwad {textWadName.Text.Trim()}");
-
-        if (textboxNPlayer.Text.Trim() != string.Empty)
-            config.Add($"-host {textboxNPlayer.Text.Trim()}");
-
-        if (textboxSkill.Text.Trim() != string.Empty)
-            config.Add($"-skill {textboxSkill.Text.Trim()}");
-
-        if (textboxLevel.Text.Trim() != string.Empty)
-            config.Add($"-warp {textboxLevel.Text.Trim()}");
-
-        if (checkDeathmatch.Checked)
-            config.Add("-deathmatch");
-
-        if (!checkMonsters.Checked)
-            config.Add("-nomonsters");
-
-        return config;
-    }
-
-    // Checks which textboxes have been set and adds their values to the command
-    private List<string> GetAllJoinInputs()
-    {
-        var config = new List<string>();
-
-        if (textboxWadJoin.Text.Trim() != string.Empty)
-            config.Add($"-iwad {textboxWadJoin.Text.Trim()}");
-
-        config.Add($"-join {textboxHostIp.Text.Trim()}");
-
-        return config;
     }
 }
